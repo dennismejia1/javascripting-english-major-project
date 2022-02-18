94fd2032971aa3880901c4b29d669a54fc1231de
dennismejia1 <82597549+dennismejia1@users.noreply.github.com>
2022-02-18 09:53:43 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 alert("scripts.js has loaded!");
+> let userInput;
+> userInput = "samosa";
+> if ( userInput === "burrito" ) {
+    console.log("Brilliant choice!");
+  } else {
+    console.log("Don’t you want a burrito?");
+  }
+//--> Don’t you want a burrito?
