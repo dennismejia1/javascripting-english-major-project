b0adf88c2491bfe484eaa7a1a4ab0ec057590cf8
dennismejia1 <82597549+dennismejia1@users.noreply.github.com>
2022-03-25 09:40:11 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -5,3 +5,23 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
       attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
       subdomains: "abcd",
       maxZoom: 18
+tileLayer.addTo(firstMap);
+firstMap.setView([40.730833, -73.9975], 16);
+let washingtonSquarePark, washingtonSquareParkMarker;
+let tenThousandth, tenThousandthPolygon, thousandth, thousandthPolyline;
+washingtonSquarePark = L.latLng(40.730833, -73.9975);
+washingtonSquareParkMarker = L.marker(washingtonSquarePark).addTo(firstMap);
+tenThousandth = [[40.7307, -73.9976], [40.7307, -73.9974], 
+                 [40.7309, -73.9974], [40.7309, -73.9976]];
+tenThousandthPolygon = L.polygon(tenThousandth, {
+                                  color: "#FFD700",
+                                  fillColor: "#fdf6e3"
+                                }
+                        ).addTo(firstMap);
+
+
+
+
+
+
+
